b57048b45bbe9566fe7cd5566df61de33b49e6fa
oscarmedina234 <oscarmedina234@gmail.com>
2023-05-31 13:04:43 -0500

    added routes and read me

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # ExpressNoteTaker
+
+## Description
+
+I wanted to be able to take starter front end code and complete the back end with the express routes. This app is a note taker where you can type out your notes and saved them to the sidebar. I learned how to use express and how to assign get, post, and delete routes. 
+
+## Installation
+
+Make sure you have all the necessary node modules installed from the package.json file with npm i 
+
+## Usage
+
+To use this app click get started from the homepage. Then either hit the plus sign at the top right, or begin typing in the Note Title field and Note Test field to start a new note. Once all fields are filled out hit the save icon to save the notes to the side bar. In the sidebar you can access your past notes and hit the trash can icon to delete them. 
+
+
+## Credits
+
+I used the in class activities to better understand the material and get some code from. 
+
+## License
+
+The license is the MIT education one. Mre info can be found in the repository. 
+
+
--- server.js
+++ server.js
@@ -3,29 +3,74 @@ const path = require('path');
 const db = require('./db/db.json');
 const fs = require('fs');
 
+let data = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-//get all notes
-app.get('/api/notes', (req, res) => res.json(db));
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Homepage route
+//give express access to the public folder 
+app.use(express.static('public'));
+
+
+// Homepage route html
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-//Notes route
+//Notes route html
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+//get all notes
+
+app.get('/api/notes', (req, res) => {
+    res.json(data);
+});
+
+//get notes by id
+app.get('/api/notes/:id', (req, res) => {
+    res.json(data[Number(req.params.id)]);
+});
+
+
+
 //post new note
 app.post('/api/notes', (req, res) => {
-    let newNotes = req.body;
-    let uniqueId = (data.lenght).toString();
-    console.log(uniqueId);
-    data.push(newNotes);
+    let userNotes = req.body;
+    let genId = (data.length).toString();
+    console.log( `The new notes ID: ${genId}`);
+    userNotes.id = genId;
+    data.push(userNotes);
+
+    fs.writeFileSync('./db/db.jason', JSON.stringify(data), err => {
+        if (err) throw (err);
+    });
+
+    res.json(data);
+});
+
+// delete note 
+
+app.delete('/api/notes/:id', (req, res) => {
+    let id = req.params.id;
+    let newId = 0;
+    console.log(`Note with ID: ${id} is being deleted`);
+    data = data.filter(thisNote => {
+        return thisNote.id != id;
+    });
+
+    for (thisNote of data) {
+        thisNote.id = newId.toString();
+        newId++;
+    }
+    fs.writeFileSync('./db/db.json', JSON.stringify(data));
+    res.json(data);
 })
 
 app.listen(PORT, () => 
